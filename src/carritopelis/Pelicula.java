31dc14e2ac496c9852c9c2888661c9f8d229b1d7
oswaldohernandez11 <oswaldo_hernandezj@hotmail.com>
2017-12-11 00:10:41 -0600

    Vercion con check Style

--- src/carritopelis/Pelicula.java
+++ src/carritopelis/Pelicula.java
@@ -7,64 +7,76 @@ package carritopelis;
  * @author Omar Ulises Hernández Cervantes, Oswaldo Josue Hernández Juárez
  */
 public class Pelicula {
-
-    String ID, titulo, actor, genero, precio; // Declaración de variables
-
+    /**
+     *  ID la instancia del codifgo de barrass.
+     */
+    private final String iD;
+    /**
+     *  titulo la instancia del ttulo de la pelicula.
+     */
+    private final String titulo;
+    /**
+     *  actor la instancia del actor de la pelicula.
+     */
+    private final String actor;
+    /**
+     *  titulo la instancia del genero de la pelicula.
+     */
+    private final String genero;
+    /**
+     *  precio la instancia del precio de la pelicula.
+     */
+    private final String precio;
     /**
      *
-     * @param ID Cadena que almacena del ID de la película
+     * @param iD Cadena que almacena del ID de la película
      * @param titulo Cadena que almacena el nombre de la película
      * @param actor Cadena que almacena el nombre del actor principal de la
      * película
      * @param genero Cadena que almacena el género al que pertenece la película
      * @param precio Cadena que almacena el precio de venta de la película
      */
-    public Pelicula(String ID, String titulo, String actor, String genero,
-            String precio) {
-        this.ID = ID;
+    public Pelicula(String iD, String titulo, String actor, String genero,
+                    String precio) {
+        this.iD = iD;
         this.titulo = titulo;
         this.actor = actor;
         this.genero = genero;
         this.precio = precio;
     }
-
-    public String getID() {
-        return ID;
+    /**
+     * Getter.
+     * @return el codigo de barras.
+    */
+    public final String getID() {
+        return iD;
     }
-
-    public String getTitulo() {
+    /**
+     * Getter.
+     * @return el titulo de la pelicula.
+    */
+    public final String getTitulo() {
         return titulo;
     }
-
-    public String getActor() {
+    /**
+     * Getter.
+     * @return el actor de la pelicula.
+    */
+    public final String getActor() {
         return actor;
     }
-
-    public String getGenero() {
+    /**
+     * Getter.
+     * @return el genero de la pelicula.
+    */
+    public final String getGenero() {
         return genero;
     }
-
-    public String getPrecio() {
+    /**
+     * Getter.
+     * @return el precio de la pelicula.
+    */
+    public final String getPrecio() {
         return precio;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setActor(String actor) {
-        this.actor = actor;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setPrecio(String precio) {
-        this.precio = precio;
     }
 }
